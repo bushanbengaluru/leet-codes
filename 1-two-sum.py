a73e774b8fbf7e8793c1ac58e2842de6644a3a28
Bushan Bengaluru <bushan@Mac.attlocal.net>
2025-01-05 14:12:21 -0500

    leetcode1

---
+++ 1-two-sum.py
@@ -0,0 +1,11 @@
+# Solution 1
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+nums = [3,3]
+target = 6
+s = Solution()
+print(s.twoSum(nums, target))
